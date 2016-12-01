91c8a9606ecf791eed49f1867fc2e9311bb01b93
Fred Anton Corvest <fac.developer@gmail.com>
2016-12-01 19:18:02 +0100

    Remove warning: multi-character character constant

--- Utils/WaveFileWriter.h
+++ Utils/WaveFileWriter.h
@@ -5,19 +5,21 @@
 // Check his blog! An amazing source of information/example about DSP. Thanks Nigel!
 
 // Modification 30/11/2016: (Fred Anton Corvest) Add filename and sampleRate parameters to writeFloatSound method
+// Modification 01/12/2016: (Fred Anton Corvest) Remove warning: multi-character character constant
 
 //
 // quick & dirty wave file
 //
 
 #include <string>
+#include <stdint.h>
 
 enum {
     /* keep sorted for wav_w64_format_str() */
-    WAVE_FORMAT_UNKNOWN					= 0x0000,		/* Microsoft Corporation */
-    WAVE_FORMAT_PCM	 					= 0x0001, 		/* Microsoft PCM format */
-    WAVE_FORMAT_MS_ADPCM				= 0x0002,		/* Microsoft ADPCM */
-    WAVE_FORMAT_IEEE_FLOAT				= 0x0003,		/* Micrososft 32 bit float format */
+    WAVE_FORMAT_UNKNOWN = 0x0000,       /* Microsoft Corporation */
+    WAVE_FORMAT_PCM = 0x0001,           /* Microsoft PCM format */
+    WAVE_FORMAT_MS_ADPCM = 0x0002,      /* Microsoft ADPCM */
+    WAVE_FORMAT_IEEE_FLOAT = 0x0003,    /* Micrososft 32 bit float format */
 };
 
 inline void writeFourCC(uint32_t val, uint8_t **bytePtr) {
@@ -49,10 +51,10 @@ void writeFloatSound(int len, float *wave, std::string& filenName, int sampleRat
     uint8_t bytes[58];
     uint8_t *bytePtr = bytes;
     
-    writeFourCC('RIFF', &bytePtr);
+    writeFourCC(0x52494646/*'RIFF'*/, &bytePtr);
     write4Bytes(4 + 26 + 12 + 8 + soundChunkLen, &bytePtr); // chunk size
-    writeFourCC('WAVE', &bytePtr);
-    writeFourCC('fmt ', &bytePtr);
+    writeFourCC(0x57415645/*'WAVE'*/, &bytePtr);
+    writeFourCC(0x666d7420/*'fmt '*/, &bytePtr);
     write4Bytes(18, &bytePtr);                              // size of subchunk that follows
     write2Bytes(WAVE_FORMAT_IEEE_FLOAT, &bytePtr);          // format
     write2Bytes(numChannels, &bytePtr);                     // number of channels
@@ -61,10 +63,10 @@ void writeFloatSound(int len, float *wave, std::string& filenName, int sampleRat
     write2Bytes(numChannels * bytesPersample, &bytePtr);    // block align
     write2Bytes(bytesPersample * 8, &bytePtr);              // bits per sample
     write2Bytes(0, &bytePtr);                               // cbSize
-    writeFourCC('fact', &bytePtr);
+    writeFourCC(0x66616374/*'fact'*/, &bytePtr);
     write4Bytes(4, &bytePtr);                               // size of subchunk that follows
     write4Bytes(numChannels * len, &bytePtr);               // size of subchunk that follows
-    writeFourCC('data', &bytePtr);
+    writeFourCC(0x64617461/*'data'*/, &bytePtr);
     write4Bytes(len * numChannels * bytesPersample, &bytePtr); // subchunk size
     
     // write array to file
